3dc8077df3d33d4f294acbc8677311b963ac40b2
PruthviKorupu <pruthvikorupu@gmail.com>
2021-12-17 21:02:14 -0800

    initial commit

---
+++ SamplePartialViewApp/Controllers/FilmController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SamplePartialViewApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SamplePartialViewApp.Controllers
+{
+    public class FilmController : Controller
+    {
+        public List<Film> _films= new();
+
+        public FilmController() 
+        {
+            _films = new List<Film>()
+            {
+            new Film() {FilmName="abc",Description="abc film description",Duration="02:20:00" },
+            new Film() {FilmName="efg",Description="efg film description",Duration="01:10:10" },
+            new Film() {FilmName="hij",Description="hij film description",Duration="01:20:00" },
+            };
+            
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.Films = _films;
+            return View();
+        }
+    }
+}
---
+++ SamplePartialViewApp/Models/Film.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SamplePartialViewApp.Models
+{
+    public class Film
+    {
+        public string FilmName { get; set; }
+        public string Duration { get; set; }
+        public string Description { get; set; }
+    }
+}
